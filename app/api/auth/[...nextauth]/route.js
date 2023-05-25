3b6dcd43ec607eefc60f4e1ca10d67824a95b842
Alp Eren Arıcı <56085269+ALERA0@users.noreply.github.com>
2023-05-26 02:10:35 +0300

    first commit

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        const userExists = await User.findOne({ email: profile.email });
+
+        if (!userExists) {
+          const newUser = new User({
+            username: profile.name.replace(" ", "").toLowerCase(),
+            email: profile.email,
+            image: profile.picture,
+          });
+
+          await newUser.save();
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
